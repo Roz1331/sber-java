e1d8e3bb915158d66ac3ce140e9b19e59487d4bd
Rose Ismailova <ismailova_roza_1@mail.ru>
2022-05-15 20:13:36 +0300

    add reading from file for hw_11

--- hw_11_multithreading/src/main/java/Main.java
+++ hw_11_multithreading/src/main/java/Main.java
@@ -1,24 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    public static List<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
+        List<Integer> list = readIntsFromFile("numbers.txt");
 
-        int n = list.size();
         for (int num : list) {
             Thread object = new Thread(new MyMultithreading(num));
 
             object.start();
         }
+    }
 
+    public static List<Integer> readIntsFromFile(String fileName) throws IOException {
+        List<Integer> res = new ArrayList<>();
 
+        try(FileReader fr = new FileReader(fileName)) {
+            Scanner scan = new Scanner(fr);
+
+            while (scan.hasNextLine()) {
+                String str = scan.nextLine();
+                Arrays.asList(str.split(" ")).forEach(x -> res.add(Integer.parseInt(x)));
+            }
+        }
+
+        return res;
     }
 }
