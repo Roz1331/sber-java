ffe9229a2ce9218b628ec70c666c9672b1b58f23
Rose Ismailova <ismailova_roza_1@mail.ru>
2022-05-18 21:40:47 +0300

    FixedThreadPool complete

--- hw_11_ThreadPool/src/main/java/FixedThreadPool.java
+++ hw_11_ThreadPool/src/main/java/FixedThreadPool.java
@@ -1,18 +1,60 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class FixedThreadPool implements ThreadPool{
 
     private int threadsNumber;
 
+    private List<Thread> threads;
+    private Queue<Runnable> queue;
+
     public FixedThreadPool(int n) {
         threadsNumber = n;
+        threads = new ArrayList<>();
+        queue = new LinkedList<>();
     }
 
     @Override
     public void start() {
-
+        for (int i = 0; i < threadsNumber; i++) {
+            Thread thread = new TaskThread();
+            thread.start();
+            threads.add(thread);
+            System.out.println(thread.getName() + " started");
+        }
     }
 
     @Override
     public void execute(Runnable runnable) {
-
+        synchronized (queue) {
+            queue.add(runnable);
+            queue.notify();
+        }
     }
+
+
+    private class TaskThread extends Thread{
+
+        @Override
+        public void run() {
+            Runnable runnable;
+            while(true) {
+                synchronized (queue) {
+                    while(queue.isEmpty()) {
+                        System.out.println(this.getName() + " is ready");
+                        try {
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    runnable = queue.remove();
+                }
+                runnable.run();
+            }
+        }
+    }
+
 }
--- hw_11_ThreadPool/src/main/java/Main.java
+++ hw_11_ThreadPool/src/main/java/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        FixedThreadPool threadPool = new FixedThreadPool(5);
+
+        threadPool.start();
+
+        for (int i = 0; i < 20; i++) {
+            threadPool.execute(new Task(i));
+        }
     }
 }
