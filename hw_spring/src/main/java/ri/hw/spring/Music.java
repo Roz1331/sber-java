6302359fe1b8772b9c32acef88531ae6e0041b23
Rose Ismailova <ismailova_roza_1@mail.ru>
2022-06-13 16:17:15 +0300

    hw spring initial commit

---
+++ hw_spring/src/main/java/ri/hw/spring/Music.java
@@ -0,0 +1,5 @@
+package ri.hw.spring;
+
+public interface Music {
+    String getSong();
+}
---
+++ hw_spring/src/main/java/ri/hw/spring/TestSpring.java
@@ -0,0 +1,15 @@
+package ri.hw.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+    }
+}
